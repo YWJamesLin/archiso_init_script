cbc51f34bd63a52ac927aa395f07d0f44b054917
YWJamesLin <YWJamesLin@gmail.com>
2019-07-23 21:54:09 +0800

    [Project] Modify to fit the package needs

--- generate.sh
+++ generate.sh
@@ -5,8 +5,8 @@
 if [ -d /usr/share/archiso ]; then
   rm -rf /usr/share/archiso/
 fi
-pacman -S archiso 
-cp -af ./airootfs packages.both /usr/share/archiso/configs/releng/
+pacman -S archiso
+cp -af ./airootfs packages.x86_64 /usr/share/archiso/configs/releng/
 sed -i 's/systemctl enable /systemctl enable nftables /' /usr/share/archiso/configs/releng/airootfs/root/customize_airootfs.sh
 cp -a ./nftables.conf /usr/share/archiso/configs/releng/airootfs/etc/
 cd /usr/share/archiso/configs/releng && ./build.sh -v
