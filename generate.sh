e6c2c1c5b4c904d5acda42be74daaf7a453207da
YWJamesLin <ywjameslin@gmail.com>
2020-09-18 09:55:55 +0800

    [Generate] Use patch to add customized packages to live environment

--- generate.sh
+++ generate.sh
@@ -6,7 +6,8 @@ if [ -d /usr/share/archiso ]; then
   rm -rf /usr/share/archiso/
 fi
 pacman -S archiso
-cp -af ./airootfs packages.x86_64 /usr/share/archiso/configs/releng/
+cp -af ./airootfs /usr/share/archiso/configs/releng/
+patch /usr/share/archiso/configs/releng/packages.x86_64 < packages.x86_64.patch
 sed -i 's/systemctl enable /systemctl enable nftables /' /usr/share/archiso/configs/releng/airootfs/root/customize_airootfs.sh
 cp -a ./nftables.conf /usr/share/archiso/configs/releng/airootfs/etc/
 cd /usr/share/archiso/configs/releng && ./build.sh -v
